7a77a04b76a04f72eb71eb1e324edb217694ba6c
Barbara Fonseca Alves <bfonseal@emeal.nttdata.com>
2023-10-05 11:25:44 -0300

    Refatorando um teste utilizando a funcionalidade lodash - Cypress._.times()

--- cypress/e2e/1-getting-started/privacy.spec.cy.js
+++ cypress/e2e/1-getting-started/privacy.spec.cy.js
@@ -1,5 +1,7 @@
-it.only('Deve testar a página da política de privacidade de forma independente', function() {
-    cy.visit('./src/privacy.html') 
-
-    cy.contains('CAC TAT - Política de privacidade').should('be.visible')
+Cypress._.times(5, function() {
+    it.only('Deve testar a página da política de privacidade de forma independente', function() {
+        cy.visit('./src/privacy.html') 
+    
+        cy.contains('CAC TAT - Política de privacidade').should('be.visible')
+    })
 })
