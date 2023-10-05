6b7dd8eb3a63774ff36c134df8e4ac6c4b1ceab3
Barbara Fonseca Alves <bfonseal@emeal.nttdata.com>
2023-10-05 12:06:15 -0300

    Acrescentando um teste utilizando a funcionalidade lodash ._repeat e a funcionalidade invoke('val')

--- cypress/e2e/1-getting-started/CAC-TAT.spec.cy.js
+++ cypress/e2e/1-getting-started/CAC-TAT.spec.cy.js
@@ -257,4 +257,11 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     })
 
 
+    it.only('Deve preencher a area de texto usando o comando invoke', function() {
+        const longText = Cypress._.repeat('teste ', 20)
+
+        cy.get('#open-text-area')
+            .invoke('val', longText) //Invoca o valor dessa área de texto e seta na variávez longText
+            .should('have.value', longText)
+    })
 })
